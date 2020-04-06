b2b9bd24e25940601a6e490258c7f10eca5a446a
rizalbayyu <38995446+rizalbayyu@users.noreply.github.com>
2020-04-06 14:49:55 +0700

    Add files via upload

---
+++ iot/BLE_BatteryService.ino
@@ -0,0 +1,63 @@
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
+#include <DHT.h>
+
+#define BatteryService BLEUUID((uint16_t)0x180F) 
+BLECharacteristic BatteryLevelCharacteristic(BLEUUID((uint16_t)0x2A19), BLECharacteristic::PROPERTY_READ | BLECharacteristic::PROPERTY_NOTIFY);
+BLEDescriptor BatteryLevelDescriptor(BLEUUID((uint16_t)0x2901));
+
+bool clientConnected = false;
+
+#define DHTTYPE    DHT11     // DHT 11
+
+class MyServerCallbacks: public BLEServerCallbacks {
+  void onConnect (BLEServer * pServer) {
+    clientConnected = true;
+  };
+  void onDisconnect (BLEServer * pServer) {
+  clientConnected = false;
+  }
+};
+
+void InitBLE() {
+  BLEDevice::init("BLE Battery");
+  // Create the BLE Server
+  BLEServer *pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pBattery = pServer->createService(BatteryService);
+
+  pBattery->addCharacteristic(&BatteryLevelCharacteristic);
+  BatteryLevelDescriptor.setValue("Percentage 0 - 100");
+  BatteryLevelCharacteristic.addDescriptor(&BatteryLevelDescriptor);
+  BatteryLevelCharacteristic.addDescriptor(new BLE2902());
+
+  pServer->getAdvertising()->addServiceUUID(BatteryService);
+
+  pBattery->start();
+  // Start advertising
+  pServer->getAdvertising()->start();
+}
+
+void setup() {
+  Serial.begin(115200);
+  Serial.println("Battery Level Indicator - BLE");
+  InitBLE();
+}
+
+uint8_t level = 57;
+
+void loop () {
+  BatteryLevelCharacteristic.setValue(&level, 1);
+  BatteryLevelCharacteristic.notify();
+  delay(5000);
+
+  level++;
+  Serial.println(int(level));
+
+  if (int(level)>=100)
+  level=100;
+}
